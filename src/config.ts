5691ff21f65a3e4638dd55fd1734a95d9d7eab24
Henil Panchal <henilp105@gmail.com>
2023-12-28 18:45:03 +0530

    fix

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import type { Site, SocialObjects } from "./types";
 export const SITE: Site = {
   website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
   author: "Henil Panchal",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
+  desc: "Henil Panchal's personal website",
   title: "Henil Panchal",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
